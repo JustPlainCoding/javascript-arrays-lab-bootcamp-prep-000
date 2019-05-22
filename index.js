d482313a7cef94d02b321323974ce42b12bf433f
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-22 23:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,26 @@ function destructivelyRemoveLastKitten() {
   return kitten;
 }
 
-function 
+function destructivelyRemoveFirstKitten() {
+  kitten.shift();
+  return kitten;
+}
+
+function appendKitten(name) {
+  var result = kitten.concat(name);
+  return result;
+}
+
+function prependKitten(name) {
+  var result = [...kitten, name];
+  return result;
+}
+
+function removeLastKitten() {
+  var result = kitten.slice(1);
+  return result;
+}
+
+function removeFirstKitten() {
+  var result = kitten.slice(0, kitten.length - 1);
+}
